527599934e31256ad4008d38bc8d948e40c45405
Ivan Juras <Ivan Juras>
2016-08-30 09:25:05 +0200

    Add meta tag for Internet Explorer compatibility

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 
 <!-- noptimize --><script>function loadCSS(href,before,media){"use strict";var ss=window.document.createElement("link");var ref=before||window.document.getElementsByTagName("script")[0];ss.rel="stylesheet";ss.href=href;ss.media="only x";ref.parentNode.insertBefore(ss,ref);setTimeout(function(){ss.media= media||"all";});return ss;}loadCSS("https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700");loadCSS("https://opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css");</script><!-- /noptimize -->
 
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 <?php wp_head(); ?>
 
 <?php HeadwayDisplay::body_open(); ?>
