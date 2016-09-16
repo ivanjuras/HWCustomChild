8516320064e862cfff857564afe15edb467e63ae
Ivan Juras <Ivan Juras>
2016-09-16 09:53:47 +0200

    Add WooCommerce specific PHP file

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
 // Headway-specific
 include 'backend/hw-specific.php';
 
+// WooCommerce specific
+include 'backend/woo-specific.php';
+
 // Add and remove styles
 include 'backend/enqueue-scripts-and-styles.php';
 include 'backend/dequeue-scripts-and-styles.php';
