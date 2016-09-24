8091ace816e9a20f1ed2d8097ca1c0aebbcd175b
Ivan Juras <Ivan Juras>
2016-09-25 00:35:32 +0200

    Add dequeue scripts

--- backend/dequeue-scripts-and-styles.php
+++ backend/dequeue-scripts-and-styles.php
@@ -2,10 +2,10 @@
 
 // ---------- Dequeue styles ---------- //
 
-add_action( 'wp_enqueue_styles', 'hwc_deregister_scripts', 9999 );
-add_action( 'wp_print_styles', 'hwc_deregister_scripts', 9999 );
-function hwc_deregister_scripts() {
-	
+add_action( 'wp_enqueue_styles', 'hwc_deregister_styles', 9999 );
+add_action( 'wp_print_styles', 'hwc_deregister_styles', 9999 );
+function hwc_deregister_styles() {
+
 	wp_dequeue_style( 'fl-builder-google-fonts' );
 	wp_dequeue_style( 'open-sans-css' );
 	wp_dequeue_style( 'font-awesome' );
@@ -15,10 +15,18 @@ function hwc_deregister_scripts() {
 	wp_dequeue_style( 'megamenu' );
 	wp_dequeue_style( 'megamenu-fontawesome' );
 	wp_dequeue_style( 'megamenu-genericons' );
-	
+
 	// Disable Yoast SEO bar on the front-end
 	if ( ! is_admin() ) {
 		wp_dequeue_style( 'yoast-seo-adminbar' );
 	}
-	
+
+}
+
+
+// ---------- Dequeue scripts ---------- //
+
+add_action( 'wp_print_scripts', 'hwc_dequeue_scripts', 9999 );
+function hwc_dequeue_scripts() {
+    wp_dequeue_script( 'responsive-menu-pro-font-awesome' );
 }
